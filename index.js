a8b6dd7c3e0574ca285f09f271353e6eb4b2afce
khanhnb <khanhnb@antsomi.com>
2023-11-17 14:03:51 +0700

    handle init home address for bot

--- index.js
+++ index.js
@@ -3,16 +3,52 @@ import { APP_CONFIG } from "./constant.js";
 import ExchangeInfoService from "./services/ExchangeInfo.js";
 import { sendCurrentTime, timeUntilNextHour } from "./utils.js";
 import { handleRunBot } from "./bot/index.js";
+import express from "express";
 
-const bot = new TelegramBot(APP_CONFIG.TOKEN, { polling: true });
+const app = express();
+app.use(express.json());
 
-bot.on("message", async (msg) => {
-  const chatId = msg.chat.id;
-  const command = msg.text.toLowerCase();
-  const payload = {
-    command,
-    bot,
-    chatId,
-  };
-  handleRunBot(payload);
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
+
+const port = process.env.PORT || 8000;
+
+app.get("/", (request, response) => {
+  response.json({
+    message: `Bot đang chay...`,
+  });
+
+  const bot = new TelegramBot(APP_CONFIG.TOKEN, { polling: true });
+
+  bot.on("message", async (msg) => {
+    const chatId = msg.chat.id;
+    const command = msg.text.toLowerCase();
+    const payload = {
+      command,
+      bot,
+      chatId,
+    };
+    handleRunBot(payload);
+  });
+});
+
+app.listen(port, () => {
+  console.log(`App using port ${port}`);
 });
