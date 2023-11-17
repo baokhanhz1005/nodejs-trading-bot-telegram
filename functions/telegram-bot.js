024a9e701589703e120ac077ab67cf36b7eb25df
khanhnb <khanhnb@antsomi.com>
2023-11-17 18:41:35 +0700

    update node_module

--- functions/telegram-bot.js
+++ functions/telegram-bot.js
@@ -2,16 +2,15 @@ import TelegramBot from "node-telegram-bot-api";
 import { APP_CONFIG } from "../constant.js";
 import { handleRunBot } from "../bot/index.js";
 
-  const bot = new TelegramBot(APP_CONFIG.TOKEN, { polling: true });
+const bot = new TelegramBot(APP_CONFIG.TOKEN, { polling: true });
 
-  bot.on("message", async (msg) => {
-    const chatId = msg.chat.id;
-    const command = msg.text.toLowerCase();
-    const payload = {
-      command,
-      bot,
-      chatId,
-    };
-    handleRunBot(payload);
-  });
-
+bot.on("message", async (msg) => {
+  const chatId = msg.chat.id;
+  const command = msg.text.toLowerCase();
+  const payload = {
+    command,
+    bot,
+    chatId,
+  };
+  handleRunBot(payload);
+});
