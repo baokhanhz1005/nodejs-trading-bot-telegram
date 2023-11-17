131b04593c3ae5125e7da777f741e85c745e1d56
khanhnb <khanhnb@antsomi.com>
2023-11-17 21:49:29 +0700

    test

--- functions/telegram-bot.js
+++ functions/telegram-bot.js
@@ -4,17 +4,6 @@ import { handleRunBot } from "../bot/index.js";
 
 export const handler = async (event, context) => {
   try {
-    const bot = new TelegramBot(APP_CONFIG.TOKEN, { polling: true });
-    bot.on("message", async (msg) => {
-      const chatId = msg.chat.id;
-      const command = msg.text.toLowerCase();
-      const payload = {
-        command,
-        bot,
-        chatId,
-      };
-      handleRunBot(payload);
-    });
     return {
       statusCode: 200,
       body: "Bot is running...",
@@ -26,3 +15,15 @@ export const handler = async (event, context) => {
     };
   }
 };
+
+const bot = new TelegramBot(APP_CONFIG.TOKEN, { polling: true });
+bot.on("message", async (msg) => {
+  const chatId = msg.chat.id;
+  const command = msg.text.toLowerCase();
+  const payload = {
+    command,
+    bot,
+    chatId,
+  };
+  handleRunBot(payload);
+});
