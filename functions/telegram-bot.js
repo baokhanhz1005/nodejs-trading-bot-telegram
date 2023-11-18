a103c443088cdaf09106c2c18e8690eaaacb7a82
khanhnb <khanhnb@antsomi.com>
2023-11-18 10:29:29 +0700

    test

--- functions/telegram-bot.js
+++ functions/telegram-bot.js
@@ -1,31 +1,9 @@
 import TelegramBot from "node-telegram-bot-api";
 import { APP_CONFIG } from "../constant.js";
 import { handleRunBot } from "../bot/index.js";
-import express from "express";
-const app = express();
-
-app.use(express.json());
-
-app.use(
-  express.urlencoded({
-    extended: true,
-  })
-);
-
-const port = process.env.PORT || 8000;
 
 export const handler = async (event, context) => {
   try {
-    app.get("/", (request, response) => {
-      response.json({
-        message: `Bot is running...`,
-      });
-    });
-
-    app.listen(port, () => {
-      console.log(`App using port ${port}`);
-    });
-
     const bot = new TelegramBot(APP_CONFIG.TOKEN);
     if (event.body) {
       const body = JSON.parse(event.body);
