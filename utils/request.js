5f14c2c189fce49b9b068445a64532702051dc82
khanhnb <khanhnb@antsomi.com>
2023-11-21 01:08:16 +0700

    ttrtrt

--- utils/request.js
+++ utils/request.js
@@ -1,14 +1,19 @@
 import axios from "axios";
 import { DOMAIN } from "../constant.js";
 
-export const callApiBinanceFt = (endpoint, method = "GET", body, other) => {
+export const callApiBinanceFt = async (
+  endpoint,
+  method = "GET",
+  body,
+  other
+) => {
   const domainBinanceFt = DOMAIN.BINANCE_FUTURE;
   const URL = `${domainBinanceFt}/${endpoint}`;
   return axios({
     method,
     url: URL,
     params: body,
-  }).catch(err => {
+  }).catch((err) => {
     console.log(err);
   });
 };
