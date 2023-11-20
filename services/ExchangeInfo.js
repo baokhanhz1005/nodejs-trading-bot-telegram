9eea5e7f02d500603b10d109bebeb15632bfbae8
khanhnb <khanhnb@antsomi.com>
2023-11-21 01:09:31 +0700

    rêpat

--- services/ExchangeInfo.js
+++ services/ExchangeInfo.js
@@ -2,7 +2,7 @@ import { ENDPOINT_TYPE } from "../constant.js";
 import { callApiBinanceFt } from "../utils/request.js";
 
 const API = {
-  info: (params) => {
+  info: async (params) => {
     const URL = `${ENDPOINT_TYPE.fAPIv1}/exchangeInfo`;
     return callApiBinanceFt(URL, "GET").then((res) => res);
   },
--- services/GetCandle.js
+++ services/GetCandle.js
@@ -2,7 +2,7 @@ import { ENDPOINT_TYPE } from "../constant.js";
 import { callApiBinanceFt } from "../utils/request.js";
 
 const API = {
-  getList: (params) => {
+  getList: async (params) => {
     const URL = `${ENDPOINT_TYPE.fAPIv1}/klines`;
     return callApiBinanceFt(URL, "GET", params.data).then((res) => res);
   },
