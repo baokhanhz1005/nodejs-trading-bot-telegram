ce4eebf7c257f3ea68cb1ed9bcc304c545ab4aa9
khanhnb <khanhnb@antsomi.com>
2023-11-21 10:49:52 +0700

    handlee

--- SendMessage.js
+++ SendMessage.js
@@ -1,9 +1,10 @@
 import axios from "axios";
+import { APP_CONFIG } from "./constant.js";
 
 const API = {
   sendMessage: async (params) => async (chat_id, text) => {
     await axios.post(
-      `https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`,
+      `https://api.telegram.org/bot${APP_CONFIG.TOKEN}/sendMessage`,
       {
         chat_id,
         text,
