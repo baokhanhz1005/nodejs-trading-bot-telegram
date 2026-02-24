bc1e700b9cb85e2f7ee7f9ff9e44d8b5c2aa5ebc
KhanhNB <baokhanhz1005@gmail.com>
2026-02-24 17:17:03 +0700

    chore: update codeeeeeee

--- app/feature/FindSignal/index.js
+++ app/feature/FindSignal/index.js
@@ -3,7 +3,13 @@ import {
   fetchApiGetCandleStickData,
   fetchApiGetListingSymbols,
 } from "../../../utils.js";
-import { getEMA } from "../../../utils/handleDataCandle.js";
+import {
+  classifyTrend,
+  getEMA,
+  getListHighest,
+  getListLowest,
+  TREND,
+} from "../../../utils/handleDataCandle.js";
 import { isDownCandle, isUpCandle } from "../../../utils/TypeCandle.js";
 
 export const FindExtremeTrending = async (payload) => {
@@ -23,7 +29,7 @@ export const FindExtremeTrending = async (payload) => {
         data: {
           symbol: symbol,
           interval: timeLine,
-          limit: LIMIT,
+          limit: LIMIT + 1,
         },
       };
 
@@ -39,44 +45,92 @@ export const FindExtremeTrending = async (payload) => {
     if (responses && responses.length) {
       for (const response of responses) {
         const { symbol: symbolCandle, data: candleStickData = [] } = response;
-
-        const [
-          forthLastCandle,
-          thirdLastCandle,
-          prevCandle,
-          lastestCandle,
-          currentCandle,
-        ] = candleStickData.slice(-5);
+        candleStickData.pop();
+        const [forthLastCandle, thirdLastCandle, prevCandle, lastestCandle] =
+          candleStickData.slice(-5);
         if (candleStickData.length < LIMIT) continue;
-        const EMA200 = getEMA(200, candleStickData);
-        /// HANDLE CONDTION FOR CHECK SIGNAL
-        if (symbolCandle === "C98USDT") {
-          console.log(
-            symbolCandle,
-            candleStickData.length,
-            EMA200,
-            lastestCandle[4]
-          );
+
+        const EMA200 = getEMA(200, candleStickData.slice(-200));
+        const EMA100 = getEMA(100, candleStickData.slice(-100));
+        const EMA50 = getEMA(50, candleStickData.slice(-50));
+        const EMA20 = getEMA(20, candleStickData.slice(-20));
+
+        const lookback = 100;
+
+        const listHighest = getListHighest(
+          candleStickData.slice(-lookback),
+          8,
+          2,
+        );
+        const listLowest = getListLowest(
+          candleStickData.slice(-lookback),
+          8,
+          2,
+        );
+
+        const highs = listHighest.map((p) => p.price);
+        const lows = listLowest.map((p) => p.price);
+        const trend = classifyTrend(highs, lows);
+        let CONDITIONS = {};
+
+        if (trend === TREND.UP) {
+          CONDITIONS = {
+            COND_1: () =>
+              candleStickData
+                .slice(-5)
+                .some(
+                  (candle) =>
+                    (+candle[3] < +EMA100 &&
+                      candleStickData
+                        .slice(-10)
+                        .every((cand) => cand[4] > EMA100)) ||
+                    (+candle[3] < +EMA200 &&
+                      candleStickData
+                        .slice(-10)
+                        .every((cand) => cand[4] > EMA200)),
+                ),
+            COND_2: () =>
+              candleStickData.slice(-5).reduce((acc, candle) => {
+                if (isDownCandle(candle)) {
+                  return acc + 1;
+                }
+
+                return acc;
+              }, 0) >= 4,
+          };
+        } else if (trend === TREND.DOWN) {
+          CONDITIONS = {
+            COND_1: () =>
+              candleStickData
+                .slice(-5)
+                .some(
+                  (candle) =>
+                    (+candle[2] > +EMA100 &&
+                      candleStickData
+                        .slice(-10)
+                        .every((cand) => cand[4] < EMA100)) ||
+                    (+candle[2] > +EMA200 &&
+                      candleStickData
+                        .slice(-10)
+                        .every((cand) => cand[4] < EMA200)),
+                ),
+            COND_2: () =>
+              candleStickData.slice(-5).reduce((acc, candle) => {
+                if (isUpCandle(candle)) {
+                  return acc + 1;
+                }
+
+                return acc;
+              }, 0) >= 4,
+          };
         }
 
-        const CONDITIONS = {
-          // COND_1: () =>
-          //   candleStickData
-          //     .slice(-5)
-          //     .some(
-          //       (candle) => isUpCandle(candle) && candle[2] / candle[1] >= 1.2
-          //     ),
-          COND_2: () =>
-            isUpCandle(lastestCandle) &&
-            lastestCandle[4] / lastestCandle[1] >= 1.015,
-        };
-        /// HANDLE CONDTION FOR CHECK SIGNAL
+        const IS_PASS_CONDITION =
+          Object.keys(CONDITIONS).length &&
+          Object.values(CONDITIONS).every((cond) => !!cond());
 
-        const IS_PASS_CONDITION = Object.values(CONDITIONS).every(
-          (cond) => !!cond()
-        );
         if (IS_PASS_CONDITION) {
-          const mess = `${buildLinkToSymbol(symbolCandle)}`;
+          const mess = `${trend} - ${buildLinkToSymbol(symbolCandle)}`;
           listMessage.push(mess);
         }
       }
@@ -86,19 +140,21 @@ export const FindExtremeTrending = async (payload) => {
   if (listMessage.length) {
     let messSend = [];
     listMessage.forEach((mess, index) => {
-      messSend.push(mess);
-      if ((index + 1) % 5 === 0 && messSend.length) {
-        bot.sendMessage(chatId, `${messSend.join("\n")}`, {
-          parse_mode: "HTML",
-          disable_web_page_preview: true,
-        });
-        messSend = [];
-      } else if (index === listMessage.length - 1 && messSend.length) {
-        bot.sendMessage(chatId, `${messSend.join("\n")}`, {
-          parse_mode: "HTML",
-          disable_web_page_preview: true,
-        });
-        messSend = [];
+      if (index <= 30) {
+        messSend.push(mess);
+        if ((index + 1) % 5 === 0 && messSend.length) {
+          bot.sendMessage(chatId, `${messSend.join("\n")}`, {
+            parse_mode: "HTML",
+            disable_web_page_preview: true,
+          });
+          messSend = [];
+        } else if (index === listMessage.length - 1 && messSend.length) {
+          bot.sendMessage(chatId, `${messSend.join("\n")}`, {
+            parse_mode: "HTML",
+            disable_web_page_preview: true,
+          });
+          messSend = [];
+        }
       }
     });
   } else {
